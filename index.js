7a50de5ad44e823f54c72545d49945e72f0b874b
Nicodemus Kiplangat <nicodemusk84@gmail.com>
2024-06-28 11:25:26 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1* num2;
+
+const random=Math.floor(Math.random()*10)
+
+let num3=24;
+let num4=5;
+let mod=num3%num4;
+
+let max = Math.max(1,2,4,7,20);
